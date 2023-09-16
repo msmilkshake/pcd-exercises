f1d04a5605b6d1f82b80d4e6831228961c6a5642
msmilkshake <prnfilipe@gmail.com>
2023-09-16 22:43:17 +0100

    Finished TicTacToe exercise 3

---
+++ src/main/java/pt/hmsk/week1/ex3/Grid.java
@@ -0,0 +1,90 @@
+package pt.hmsk.week1.ex3;
+
+import javax.security.auth.callback.Callback;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Grid {
+    private JFrame frame = new JFrame();
+    private JLabel[][] cells;
+
+    private int[] lastClick = new int[]{-1, -1};
+
+    public Grid(String name, int rows, int cols, int length) {
+        validateArgs(rows, cols, length);
+        frame.setTitle(name);
+
+        buildGrid(rows, cols, length);
+
+        frame.pack();
+    }
+
+    private void buildGrid(int rows, int cols, int length) {
+        cells = new JLabel[rows][cols];
+        Dimension square = new Dimension(length, length);
+        frame.setLayout(new GridLayout(rows, cols));
+
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                JLabel cell = buildCell(square);
+                cells[row][col] = cell;
+                registerAction(cell, row, col);
+                
+                frame.add(cell);
+            }
+        }
+    }
+
+    private void registerAction(JLabel cell, int row, int col) {
+        cell.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                lastClick[0] = row;
+                lastClick[1] = col;
+                callback();
+            }
+        });
+    }
+
+    public void callback() {
+        JOptionPane.showMessageDialog(null,
+                "click on label " + lastClick[0] + "," + lastClick[1]);
+    }
+
+    private JLabel buildCell(Dimension square) {
+        JLabel cell = new JLabel();
+        cell.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        cell.setPreferredSize(square);
+        return cell;
+    }
+
+    public void validateArgs(int rows, int cols, int length) {
+        if (rows <= 0) {
+            throw new RuntimeException("Number of rows must be a positive int.");
+        }
+        if (cols <= 0) {
+            throw new RuntimeException("Number of cols must be a positive int.");
+        }
+        if (length <= 0) {
+            throw new RuntimeException("Length of cells must be a positive int.");
+        }
+    }
+
+    public void open() {
+        frame.setVisible(true);
+    }
+
+    public int[] getLastClick() {
+        return lastClick;
+    }
+
+    public JLabel getCell(int row, int col) {
+        return cells[row][col];
+    }
+
+    public static void main(String[] args) {
+        new Grid("Test", 5, 4, 50).open();
+    }
+}
---
+++ src/main/java/pt/hmsk/week1/ex3/TicTacToe.java
@@ -0,0 +1,78 @@
+package pt.hmsk.week1.ex3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TicTacToe extends Grid {
+    
+    private String player = "X";
+    public TicTacToe(int length) {
+        super("TicTacToe", 3, 3, length);
+        setGridStyle();
+    }
+
+    private void setGridStyle() {
+        for (int row = 0; row < 3; ++row) {
+            for (int col = 0; col < 3; ++col) {
+                JLabel cell = getCell(row, col);
+                cell.setFont(new Font("Arial", Font.BOLD, 42));
+                cell.setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            
+        }
+    }
+
+    @Override
+    public void callback() {
+        play();
+    }
+    
+    private void play() {
+        JLabel cell = getCell(getLastClick()[0], getLastClick()[1]);
+        if (cell.getText().isEmpty()) {
+            cell.setText(player);
+            if (!checkWinner()) {
+                player = player.equals("X") ? "O" : "X";
+            }
+        }
+    }
+
+    private boolean checkWinner() {
+        boolean winner = false;
+        
+        // Check rows:
+        int rowPoints = 0;
+        int colPoints = 0;
+        int diag1Points = 0;
+        int diag2Points = 0;
+        for (int row = 0; row < 3; ++row) {
+            for (int col = 0; col < 3; ++col) {
+                if (getCell(row, col).getText().equals(player)) {
+                    ++rowPoints;
+                }
+                if (getCell(col, row).getText().equals(player)) {
+                    ++colPoints;
+                }
+            }
+            if (getCell(row, row).getText().equals(player)) {
+                ++diag1Points;
+            }
+            if (getCell(row, 2 - row).getText().equals(player)) {
+                ++diag2Points;
+            }
+            rowPoints = rowPoints == 3 ? 3 : 0;
+            colPoints = colPoints == 3 ? 3 : 0;
+        }
+        winner = rowPoints == 3 || colPoints == 3 || diag1Points == 3 || diag2Points == 3;
+        
+        if (winner) {
+            JOptionPane.showMessageDialog(null, "Player " + player + " Wins!");
+        }
+        
+        return winner;
+    }
+
+    public static void main(String[] args) {
+        new TicTacToe(100).open();
+    }
+}
