c81368bd3fdd261e59c26ef09e2627e321771366
msmilkshake <prnfilipe@gmail.com>
2023-09-20 22:07:50 +0100

    week 2

--- src/main/java/pt/hmsk/week2/ex5/PongBoard.java
+++ src/main/java/pt/hmsk/week2/ex5/PongBoard.java
@@ -2,13 +2,27 @@ package pt.hmsk.week2.ex5;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class PongBoard extends JPanel {
-    private Dimension minSize = new Dimension(640, 800);
-    JPanel gameContent = new JPanel();
+    private Dimension minSize = new Dimension(480, 560);
+    private Dimension playerSize = new Dimension(60, 10);
+    
+    private JPanel gameContent = new JPanel();
+    private JLabel player1 = new JLabel();
+    private JLabel player2 = new JLabel();
+    
+    private boolean aPressed = false;
+    private boolean dPressed = false;
+    private boolean lPressed = false;
+    private boolean rPressed = false;
+    
+    private int speed = 6;
     
     public PongBoard() {
         addContent();
+        gameLoop();
     }
 
     private void addContent() {
@@ -21,16 +35,89 @@ public class PongBoard extends JPanel {
         gbc.fill = GridBagConstraints.BOTH;
         gbc.insets = new Insets(0, 12, 0, 12);
         gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
         
-        gameContent.setLayout(new GridBagLayout());
-        gameContent.setBackground(Color.BLACK);
+        gameContent.setLayout(null);
+        gameContent.setBackground(Color.LIGHT_GRAY);
+        gameContent.setBorder(BorderFactory.createMatteBorder(0, 4, 0, 4, Color.black));
+        gameContent.setPreferredSize(minSize);
+        
+        player1.setBackground(Color.BLUE);
+        player1.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        player1.setPreferredSize(playerSize);
+        player1.setOpaque(true);
+        
+        player2.setBackground(Color.RED);
+        player2.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        player2.setPreferredSize(playerSize);
+        player2.setOpaque(true);
+        int x = minSize.width / 2 - 4 - playerSize.width / 2;
+        int y1 = 10;
+        int y2 = minSize.height - playerSize.height - 10;
+        player1.setBounds(x, y1, 60, 10);
+        player2.setBounds(x, y2, 60, 10);
+        
+        gameContent.add(player1);
+        gameContent.add(player2);
+        
         
         add(gameContent, gbc);
     }
+    
+    private void gameLoop() {
+        Timer timer = new Timer(20, e -> {
+            handleMovement();
+            repaint();
+        });
+        timer.start();
+    }
+    
+    private void handleMovement() {
+        handlePlayerMovement(player1, aPressed, dPressed);
+        handlePlayerMovement(player2, lPressed, rPressed);
+    }
 
+    private void handlePlayerMovement(JLabel player, boolean left, boolean right) {
+        int x = player.getX();
+        if (left && x > 4) {
+            x -= speed;
+            player.setLocation(x, player.getY());
+        }
+        if (right && x < minSize.width - playerSize.width - 4 - 24) {
+            x += speed;
+            player.setLocation(x, player.getY());
+        }
+    }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    public void handleKeyPress(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_A && !aPressed) {
+            aPressed = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D && !dPressed) {
+            dPressed = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT && !lPressed) {
+            lPressed = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT && !rPressed) {
+            rPressed = true;
+        }
+    }
+
+    public void handleKeyRelease(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_A && aPressed) {
+            aPressed = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D && dPressed) {
+            dPressed = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT && lPressed) {
+            lPressed = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT && rPressed) {
+            rPressed = false;
+        }
     }
 }
--- src/main/java/pt/hmsk/week2/ex5/PongGame.java
+++ src/main/java/pt/hmsk/week2/ex5/PongGame.java
@@ -1,19 +1,39 @@
 package pt.hmsk.week2.ex5;
 
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class PongGame {
-    JFrame frame = new JFrame("P0nG");
+    private JFrame frame = new JFrame("P0nG");
+    private PongBoard pb = new PongBoard();
     
     public PongGame() {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.add(new PongBoard());
+        frame.setResizable(false);
+        frame.add(pb);
+        setupKeyListener();
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void setupKeyListener() {
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                pb.handleKeyPress(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                pb.handleKeyRelease(e);
+            }
+        });
+        frame.setFocusable(true);
+        frame.requestFocusInWindow();
     }
 
     public static void main(String[] args) {
         new PongGame();
     }
-    
 }
